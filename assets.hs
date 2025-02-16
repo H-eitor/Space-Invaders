b6bb7bed9c2d6aa24aea39c3f14c5f00ef0779bb
Heitor de Souza Alves <heitor.souza.alves@ccc.ufcg.edu.br>
2025-02-16 19:15:09 -0300

    Remoção de arquivos desnecessários

--- assets.hs
+++
@@ -1,6 +0,0 @@
-module Assets where
-    import Graphics.Gloss
-    import Graphics.Gloss.Interface.Pure.Game
-    
-    drawPlayer :: Float -> Picture
-    drawPlayer x = translate x (-250) (color blue (rectangleSolid 50 20))
