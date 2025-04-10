e5275af4ba883e01d9221fd223777eb61a3e8d6d
Isaque Esdras Rocha <140713325+isaque48@users.noreply.github.com>
2025-04-10 18:12:07 -0300

    ajustes

--- prolog/jogoProlog.pl
+++ prolog/jogoProlog.pl
@@ -8,6 +8,7 @@
 :- include('bullet.pl').
 :- include('gamePhase.pl').
 
+% FAtos dinamicos do jogo
 :- dynamic player/1, bullets/1, enemies/1, enemy_bullets/1, enemy_direction/1, timer/1, shields/1, lives/1, lives_display/1, last_shot_time/1, shoot_cooldown/1,
 current_phase/1, max_phases/1, boss_defeated/0, boss_health/1, enemy_down_timer/1, enemy_shoot_timer/1.
 
@@ -21,12 +22,11 @@ start :-
     send(Window, scrollbars, none),
     send(Window, open_centered),
 
-    % Inicializa vidas
     retractall(lives(_)),
     assert(lives(3)),
 
     retractall(current_phase(_)),
-    assert(current_phase(1)),
+    assert(current_phase(5)),
     retractall(max_phases(_)),
     assert(max_phases(5)),
     retractall(boss_defeated),
@@ -37,22 +37,18 @@ start :-
     send(Window, display, LivesText, point(20, 580)),
     assert(lives_display(LivesText)),
 
-    % TESTE
     retractall(username(_)),
     assert(username(UserName)),
 
-    % Inicializa pontuação
     retractall(score(_)),
     assert(score(0)),
 
-    % Exibe a pontuação na tela
     new(ScoreText, text('Pontos: 0')),
     send(ScoreText, font, font(arial, bold, 25)),
     send(ScoreText, colour, white),
     send(Window, display, ScoreText, point(650, 580)),
     assert(score_display(ScoreText)),
 
-    % Exibe o high score na tela
     get_highscore(TopUser, TopScore),
     format(atom(HighScoreText), 'Recorde: ~d Pontos, por ~w ', [TopScore, TopUser]),
     new(HighText, text(HighScoreText)),
@@ -60,7 +56,6 @@ start :-
     send(HighText, colour, white),
     send(Window, display, HighText, point(280, 10)),
 
-    % Cria o jogador
     new(Player, box(70, 20)),
     send(Player, fill_pattern, colour(green)),
     send(Player, move, point(360, 550)),
@@ -68,7 +63,6 @@ start :-
     retractall(player(_)),
     assert(player(Player)),
 
-    % Inicializa as listas e variáveis de estado
     retractall(bullets(_)),
     retractall(enemies(_)),
     retractall(enemy_bullets(_)),
@@ -78,21 +72,16 @@ start :-
     assert(enemies([])),
     assert(enemy_bullets([])),
     assert(enemy_direction(right)),
-    assert(shields([])), % Lista de escudos
+    assert(shields([])), 
 
-    % Cria os escudos de proteção
     create_shields(Window),
-
-    % Cria os inimigos
     create_enemies(Window),
 
-    % Inicializa o sistema de cooldown
     retractall(last_shot_time(_)),
     retractall(shoot_cooldown(_)),
     assert(shoot_cooldown(0.8)),  
     assert(last_shot_time(0)),
 
-    % Timers para atualização do jogo
     new(BulletTimer, timer(0.01, message(@prolog, update_bullets, Window))),
     send(BulletTimer, start),
     
@@ -104,32 +93,30 @@ start :-
     new(EnemyShootTimer, timer(ShootInterval, message(@prolog, enemy_shoot, Window))),
     send(EnemyShootTimer, start),
 
-    % Corrigido: parêntese fechado corretamente
     new(CollisionTimer, timer(0.1, message(@prolog, check_enemies_position, Window))),
     send(CollisionTimer, start),
     assert(timer(CollisionTimer)),
     
-    % Timer para atualizar balas dos inimigos
     new(EnemyBulletTimer, timer(0.01, message(@prolog, update_enemy_bullets, Window))),
     send(EnemyBulletTimer, start),
 
     current_phase(Phase),
     descida_intervalo(Phase, Interval),
     (Interval > 0 ->
-        retractall(enemy_down_timer(_)), % Remove qualquer timer existente
+        retractall(enemy_down_timer(_)), 
         new(EnemyDownTimer, timer(Interval, message(@prolog, move_enemies_down, Window))),
         send(EnemyDownTimer, start),
         assert(enemy_down_timer(EnemyDownTimer))
     ; true),
 
-    % Configura controles do teclado
     send(Window, recogniser, new(K, key_binding(@nil, argument))),
     send(K, function, 'cursor_left', message(@prolog, move_left, Player, Window)),
     send(K, function, 'cursor_right', message(@prolog, move_right, Player, Window)),
     send(K, function, 'SPC', message(@prolog, player_shoot, Player, Window)),
-    send(K, function, '\\e', message(@prolog, game_over, Window)). % TESTE
+    send(K, function, '\\e', message(@prolog, game_over, Window)). 
 
 
+%Timer atual do jogo
 current_timer(Timer) :-
     timer(Timer),
     object(Timer).
@@ -139,7 +126,7 @@ read_string(String) :-
    read_line_to_codes(Input, Codes),
    string_codes(String, Codes).
 
-% Auxiliar para limpar lista de balas
+% Limpa a lista de balas
 clean_bullet_list([]).
 clean_bullet_list([Bullet|Rest]) :-
     (object(Bullet) -> free(Bullet); true),
