600cb4d1a4001520a76edf1b14b958305fc96a39
Isaque Esdras Rocha <140713325+isaque48@users.noreply.github.com>
2025-04-10 18:19:38 -0300

    ajustes

--- prolog/score.pl
+++ prolog/score.pl
@@ -1,12 +1,14 @@
 :- use_module(library(csv)).
 :- use_module(library(pairs)).
 
+% Atualiza o placar
 update_score(NewScore) :-
     retract(score(_)),
     assert(score(NewScore)),
     score_display(ScoreDisplay),
     send(ScoreDisplay, string, string('Pontos: %d', NewScore)).
 
+% Salva o placar em um arquivo .csv
 save_score_to_csv(UserName, Score) :-
     open('scores.csv', append, Stream),
     atomic_list_concat([UserName, Score], ',', Linha),
@@ -14,6 +16,7 @@ save_score_to_csv(UserName, Score) :-
     nl(Stream),
     close(Stream).
 
+% Define e retorna os 5 maiores placares registrados
 highscore(Window, X, Y) :-
     csv_read_file('scores.csv', Linhas, [functor(row)]),
     findall(Score-UserName, member(row(UserName, Score), Linhas), Pairs),
@@ -23,6 +26,7 @@ highscore(Window, X, Y) :-
     take(5, Highscore, Top5),
     display_highscore(Top5, Window, X, Y).
 
+% Exibe os placares na tela
 display_highscore([], _, _, _).
 display_highscore([UserName-Score|Rest], Window, X, Y) :-
     atomic_list_concat([UserName, ': ', Score], Texto),
@@ -47,6 +51,7 @@ comparar_pares(Ord, Key1-Value1, Key2-Value2) :-
         compare(Ord, Key, Key)
     ).
 
+% Funcao que retorna o maior placar registrado
 get_highscore(UserName, Score) :-
 csv_read_file('scores.csv', Linhas, [functor(row)]),
 findall(S-U, member(row(U, S), Linhas), Pairs),
